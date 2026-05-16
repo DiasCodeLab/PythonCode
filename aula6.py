7e6334375b81db12c8026ce347029484cfd877d0
Mateus Dias <cptmert3@gmail.com>
2026-05-15 21:18:56 -0300

    Variaveis com tipo de dados

---
+++ aula6.py
@@ -0,0 +1,11 @@
+'''
+Variaveis com tipos de dados 
+
+'''
+
+nome = 'Mateus'
+idade = 15
+maior_de_idade = idade >= 18
+
+print('nome:',nome,'idade:', idade)
+print(nome ,'é maior de idade ?', maior_de_idade)
